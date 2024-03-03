c378c24399dd5a428dd5d932daa990a6f5088f1b
lucdiazs <salinasdiazluciano@gmail.com>
2024-03-03 19:58:36 -0300

    Se arregla el aspecto visual

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,9 +6,9 @@ const Card = ({ imagen, nombre, tipo, genero, desc_personalidad, region, comuna 
       <img src={imagen} alt={nombre} className="card-img" />
       <h2>{nombre}</h2>
       <h3>{tipo}</h3>
-      <h3>{genero}</h3>
-      <p>{desc_personalidad}</p>
-      <p>{region}, {comuna}</p>
+      <h4>{genero}</h4>
+      {desc_personalidad}
+      <h2>{region}, {comuna}</h2>
     </div>
   );
 };
